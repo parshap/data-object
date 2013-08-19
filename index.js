ded27c96c716fb337800fefe150f3c45ef9a1806
Parsha Pourkhomami <parshap+git@gmail.com>
2013-08-18 20:43:17 -0700

    Move files out of nested directory

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Export Model
-var Model = module.exports = require("./model");
+var Model = module.exports = require("./lib");
 
 // Run Model through all "mixin" functions to add functionality
 require("./events")(Model);
